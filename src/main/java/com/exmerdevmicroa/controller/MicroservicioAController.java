b93cb4dbfa7c887b5937bf1ea7f5f4371e39ae12
elivar <elargor@gmail.com>
2021-01-02 12:43:06 -0500

    funcionalidades de encriptado de la clave

--- src/main/java/com/exmerdevmicroa/controller/MicroservicioAController.java
+++ src/main/java/com/exmerdevmicroa/controller/MicroservicioAController.java
@@ -2,6 +2,8 @@ package com.exmerdevmicroa.controller;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.exmerdevmicroa.model.DatosMicroservicioA;
 import com.exmerdevmicroa.model.RequestMicroservicioA;
 import com.exmerdevmicroa.model.RequestMicroservicioB;
+import com.exmerdevmicroa.model.Response;
 import com.exmerdevmicroa.service.IDatosMicroservicioAService;
 import com.exmerdevmicroa.service.MessageMicroservicioBService;
 
@@ -20,23 +23,28 @@ public class MicroservicioAController {
 
 	@Autowired
 	private IDatosMicroservicioAService microServicioAService;
-	
+
 	@Autowired
 	private MessageMicroservicioBService messageMicroservicioBService;
 
-	
-	@PostMapping("requestmicroa")
-	public void callMicroA(@RequestBody RequestMicroservicioA requestMicroA) {
-		//guardar los datos solo para MA
-		DatosMicroservicioA datosMA=new DatosMicroservicioA(null, requestMicroA.getEmail(), requestMicroA.getPass());
-		//guardar los datos solo para MB
-		RequestMicroservicioB requestDatosMB= new RequestMicroservicioB(requestMicroA.getName(), requestMicroA.getLastname(), requestMicroA.getPhone());
+	// para codificar y decodificar la clave
+	BCryptPasswordEncoder passEncode = new BCryptPasswordEncoder();
 
-		microServicioAService.save(datosMA);
+	@PostMapping("requestmicroa")
+	public ResponseEntity<Object> callMicroA(@RequestBody RequestMicroservicioA requestMicroA) {
+		// guardar los datos solo para MA
+		DatosMicroservicioA datosMA = new DatosMicroservicioA(null, requestMicroA.getEmail(), requestMicroA.getPass());
+		// guardar los datos solo para MB
+		RequestMicroservicioB requestDatosMB = new RequestMicroservicioB(requestMicroA.getName(),
+				requestMicroA.getLastname(), requestMicroA.getPhone());
 		
+		datosMA.setPass(passEncode.encode(datosMA.getPass()));
+		microServicioAService.save(datosMA);
+
 		// llamado y envio de datos al microservicio b
 		messageMicroservicioBService.sendMessage(requestDatosMB);
-		
+		return ResponseEntity.ok(new Response("ok"));
+
 	}
 
 }
--- src/main/java/com/exmerdevmicroa/service/MessageMicroservicioBService.java
+++ src/main/java/com/exmerdevmicroa/service/MessageMicroservicioBService.java
@@ -1,8 +1,15 @@
 package com.exmerdevmicroa.service;
 
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,20 +21,32 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class MessageMicroservicioBService {
 	Logger logger = LoggerFactory.getLogger(MessageMicroservicioBService.class);
-	private final ObjectMapper mapper = new ObjectMapper() ;
+	private ObjectMapper mapper = new ObjectMapper();
 	
+	
+
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
-	
-	public void sendMessage (RequestMicroservicioB datosMicroservicioB) {
+
+	public void sendMessage(RequestMicroservicioB datosMicroservicioB) {
+		CorrelationData cd1 = new CorrelationData();
 		logger.info("Enviando a microservicio B...", datosMicroservicioB.getName());
 		try {
-			rabbitTemplate.convertAndSend("CREATED.DATOS.MICROSERVICESB", mapper.writeValueAsString(datosMicroservicioB));
+			rabbitTemplate.convertAndSend("","CREATED.DATOS.MICROSERVICESB",
+					mapper.writeValueAsString(datosMicroservicioB),cd1);
 		} catch (AmqpException | JsonProcessingException e) {
 			e.printStackTrace();
-		}
+		}			
+	
+		logger.info("Respuesta....");
+		
+		/*try {
+			System.out.println(cd1.getFuture().get(60, TimeUnit.SECONDS).isAck());
+		} catch (InterruptedException | ExecutionException | TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		//System.out.println(correlationData.getReturnedMessage());
 	}
-	
-	
 
 }
